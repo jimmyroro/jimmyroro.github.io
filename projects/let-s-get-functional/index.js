38ebc29e0dc4ae8b04b6c594bb4ebe8ce3c035fe
Ubuntu <ubuntu@ip-172-31-17-4.ec2.internal>
2020-03-02 22:00:40 +0000

    let's get functional completed

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -160,25 +160,67 @@ var friendFirstLetterCount = function(array,customer,letter) {
 
 
 var friendsCount = function(array, name) {
-    let newArray = [];
+    //we create an array the we will push values into and return
+    let friendOf = [];
+    //we will use map to loop through our array 
+    _.map(array, function(element, index, array) {
+        //we use pluck to access each customers' list of friends, and define friends to be an array of the customer's friends' names
+        let friends = _.pluck(element.friends, "name");
+        //if the <name> we are given appears on our <friends> array, then we want to push that customer's name into our <friendOf> array
+        if (friends.includes(name)) {
+            friendOf.push(element.name);
+        }
+    });
+    //return our updated friendOf list
+    return friendOf;
 };
-
-console.log(friendsCount(customers, "Greta Wells"));
 
 var topThreeTags = function(array) {
     //create an array with all the tags in it, using a double filter
     let allTags = [];
+    //the first filter accesses each customer object in our customers array
     _.filter(array, function (customer, index, customers) {
+        //this second filter access the tags array within each customer
         _.filter (customer.tags, function (element, index2, array) {
+            //we push all the tags from all the customers into an <allTag> array
             allTags.push(element);
         });
     });
-    let uniqueTags = _.unique(allTags);
+    //here we are going to create an object that will have all the tags and their counts as key/value pairs
+    let tagObject = {};
+    //using reduce on our allTags array to update the <tagObject>
+    _.reduce(allTags, function(result, element, index){
+        //if our <tagObject> doesn't have a key of <element> (a particular tag), then we will give it one and set its value to 0
+        if(tagObject[element] === undefined) {
+            tagObject[element] = 0;
+            //if our <tagObject> does have a key of <element>, then that means we want to increase its value to reflect another instance of a 
+            //particular tag
+        } else (tagObject[element] = tagObject[element]+1);
+    });
+    //we are creating a topTags array which will hold the names of the particular tags, we will eventually return it
+    let topTags = [0,0,0];
+    //creating a topTagsNumbers array, which we will use to compare to values in our <tabObject>
+    let topTagsNumbers = [0,0,0];
+    //within this map of <tagObject>, we are checking each <value> inside tagObject. if it is bigger than any of the values in <topTagsNumbers> then 
+    //we will update the <topTagsNumbers> with that value and also the <topTags> with the key, making sure to keep the positions linked. so ultimately,
+    //topTagsNumbers will hold the three highest values, because as each new value is assessed it will potentially replace one of our arrays,
+    //while <topTags> will hold the matching keys of those values
+    _.map(tagObject, function(value, key, tagObject){
+        if(value > topTagsNumbers[0]) {
+            topTags[0] = key;
+            topTagsNumbers[0] = value;
+        } else if (value > topTagsNumbers[1]){
+            topTags[1] = key;
+            topTagsNumbers[1] = value;
+        } else if (value > topTagsNumbers[2]) {
+            topTags[2] = key;
+            topTagsNumbers[2] = value;
+        }
+    });
+    //now we can return our <topTags> array
+   return topTags;
     
 };
-console.log(customers[0].friends);
-let friendName = _.pluck(customers[0].friends, "name");
-console.log(friendName);
 
 var genderCount = function(array) {
     //create an object that we will update and return
