794c9c871a69dab8a0be2d413f21de766b386063
Richard Rosier <jimmyroro58@gmail.com>
2020-09-24 10:13:25 -0500

    beutify css

--- website/src/Components/Home.jsx
+++ website/src/Components/Home.jsx
@@ -4,20 +4,14 @@ import {Link} from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div class="container" id="bio-body">
+    <div class="container">
         <div class="row">
-          <div class="col-sm">
-            <img src={profile} alt="me"></img>
-          </div>
-          <div class="col-sm">
-            <div class="row">
-              I'm a software developer looking for my next project. I've worked front end and backend, and am always up to learn new things. Check out my latest projects below, and don't hesitate to reach out to me!
-            </div>
-            <div class="row">
-              <Link to="./projects">link to projects? pictures of them or something?</Link>
-            </div>
-          </div>
+            <img id="selfie" src={profile} alt="me"></img>
+            <p id="intro">
+              I'm a software developer looking for my next project. I've worked front end and backend, and am always up to learn new things. Check out my latest projects, and don't hesitate to reach out to me!
+            </p>
         </div>
+
       </div>
   )
 }
--- website/src/Components/Projects.jsx
+++ website/src/Components/Projects.jsx
@@ -3,32 +3,44 @@ import upcloseLogo from '../pictures/upcloseLogo.svg';
 import crawlLogo from '../pictures/crawlLogo.png';
 import organizeLogo from '../pictures/organizeLogo.png';
 
+const mist = {
+  backgroundColor: "#90AFC5",
+}
+
 const Projects = () => {
   return (
     <div class="card-group">
-      <div class="card">
+      <div class="card" style={mist}>
         <img class="card-img-top h-50" src={upcloseLogo} alt="Logo for Upclose" />
         <div class="card-body">
-          <h5 class="card-title">UpClose</h5>
+          <h5 class="card-title text-center">UpClose</h5>
           <p class="card-text">Upclose is a website my team and I developed from greenfield to deployment. It allows users to create a party and invite people to join it. Once inside a party, WebRTC and socket.io allow users to see and hear each other. There is a spatial aspect too, where audio diminishes as you get farther away from other users’ avatars.  I worked in the fronted and backend, using React hooks to build out the frontend and authenticated users with Google Login, saving the users and their parties to a remote MYSQL database. I learned how to integrate Typescript with React.js, and how to style with Tailwind instead of traditional CSS.</p>
-          <a href="https://upclose.uc.r.appspot.com/" class="btn btn-primary">Website</a>
-          <a href="https://github.com/HearTogether/upclose" class="btn btn-primary">Repo</a>
+          <button href="https://upclose.uc.r.appspot.com/">
+            Website
+          </button>
+          <button href="https://github.com/HearTogether/upclose">
+            Repo
+          </button>
         </div>
       </div>
-      <div class="card">
+      <div class="card" style={mist}>
         <img class="card-img-top h-50" src={organizeLogo} alt="Logo for Upclose" />
         <div class="card-body">
-          <h5 class="card-title">Organize power</h5>
+          <h5 class="card-title text-center">Organize power</h5>
           <p class="card-text">Organize power is a website that my team and I modified. It is a platform for creating movements that people can join and learn about, and was built with React.js, Node, Express, and MySQL. We added user stories to it, including messaging, linking charities, and creating events in a calendar.</p>
-          <a href="https://github.com/Apptonomy/organizepower" class="btn btn-primary">Repo</a>
+          <button href="https://github.com/Apptonomy/organizepower"> 
+            Repo
+          </button>
         </div>
       </div>
-      <div class="card">
+      <div class="card" style={mist}>
         <img class="card-img-top h-50" src={crawlLogo} alt="Logo for Upclose" />
         <div class="card-body">
-          <h5 class="card-title">Crawl</h5>
-          <p class="card-text">Crawl is a website that allows you to create a pub crawl and keep track of your buddies. We created it using Vue.js, Node, Express, and MySQL. We used Google to authenticate users, and also to allow users to search on a map for bars to save for their crawl.</p>
-          <a href="https://github.com/Apptonomy/Crawl" class="btn btn-primary">Repo</a>
+          <h5 class="card-title text-center">Crawl</h5>
+          <p class="card-text">Crawl is a website that allows you to create a pub crawl and keep track of your buddies. We created it using Vue.js, Node, Express, and MySQL. We used Google to authenticate users, access and save the user's location, and search for bars near them.</p>
+          <button href="https://github.com/Apptonomy/Crawl">
+            Repo
+          </button>
         </div>
       </div>
     </div>
