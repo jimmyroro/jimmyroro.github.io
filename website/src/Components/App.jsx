f4fd7d4c4110af5312d296508cc34457abc86da5
jimmyroro <jimmyroro58@gmail.com>
2020-12-02 18:24:12 -0600

    change Routing to happen in App instead of Navbar

--- website/src/Components/App.jsx
+++ website/src/Components/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import Navbar from './Navbar';
 import '../App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact'
 
 
 function App() {
   return (
     <div className="App" >
-      <Navbar/>
+      <Router>
+        <Navbar/>
+          <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- website/src/Components/Navbar.jsx
+++ website/src/Components/Navbar.jsx
@@ -1,42 +1,25 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
-import Contact from './Contact';
-import Home from './Home';
-import Projects from './Projects'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <Router>
       <div class="container-fluid" id="navbar">
           <div class="row">
             <div class="col-sm">
               <Link to="/">Bio</Link>
             </div>
             <div class="col-sm">
-              <a href="https://www.linkedin.com/in/richard-james-rosier/">LinkedIn</a>
+              <a href="https://www.linkedin.com/in/richard-james-rosier/" target="blank" rel="noopener noreferrer">LinkedIn</a>
             </div>
             <div class="col-sm">
-              <a href="https://github.com/jimmyroro">Github</a>
+              <a href="https://github.com/jimmyroro" target="blank" rel="noopener noreferrer">Github</a>
             </div>
             <div class="col-sm">
               <Link to="/contact">Contact</Link>
             </div>
           </div>
         </div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-    </Router>
+        
   )
 }
 
