0fde0be06d868bee6e7b3f1e1bb024fea6334a7a
jimmyroro <jimmyroro58@gmail.com>
2020-12-15 09:59:31 -0600

    changed about bio

--- website/src/Components/Home.jsx
+++ website/src/Components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         </div>
         <div className="col flex">
           <p id="intro">
-            I'm a software developer looking for my next project. I've worked front end and backend, and am always up to learn new things. Check out my latest projects below, and don't hesitate to reach out to me!
+            I'm a software developer who enjoys creating things, whether that be elaborate Mardi Gras costumes, clean code, or responsive U/X. I've worked front end and backend, and am always up to learn new things. In my previous life I served food, now I serve web pages. Check out my latest projects below, and link up with me if you’d like to chat!
           </p>
         </div>
       </div>
